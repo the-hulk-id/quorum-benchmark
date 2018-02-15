372ff16e95931f93cf14b953d5d195743fdc34d7
Sakkarin Saysai <sakkarin@blockfint.com>
2018-02-15 12:44:31 +0700

    add key6

--- 3_runNode.sh
+++ 3_runNode.sh
@@ -15,6 +15,10 @@ jq -c '.[] | { id, user, ip}' server.json | while read i; do
   # scp -r $PWD/node/start.sh $USER@$IP:~/ethereum-benchmark/node/start.sh
   # scp -r $PWD/node/genesis.json $USER@$IP:~/ethereum-benchmark/node/genesis.json
   # scp -r $PWD/node/permissioned-nodes.json $USER@$IP:~/ethereum-benchmark/node/permissioned-nodes.json
+
+  scp -r $PWD/quorum_Node/permissioned-nodes.json $USER@$IP:~/ethereum-benchmark/quorum_Node/permissioned-nodes.json
   ssh -n $USER@$IP "docker rm quorum -f"
   ssh -n $USER@$IP "cd ethereum-benchmark/node/ && docker run -d -e 'NODE=$ID' --name quorum -p 22000:22000 -p 21000:21000 -p 54000:54000 -v ~/ethereum-benchmark/quorum_Node:/quorum_script quorum"
 done
+
+sleep 10
